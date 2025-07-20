9608115286c14c6fc2777676f0ef4b756f56d6fc
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-07-21 00:28:24 +0600

    Display employees dynamic data on employee list page

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    static public function getRecord() {
+        $return = self::select('users.*')->orderBy('id', 'DESC')->paginate(20);
+
+        return $return;
+    }
 }
