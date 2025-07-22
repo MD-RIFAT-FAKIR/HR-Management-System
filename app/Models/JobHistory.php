425a1e660936da334c3d8480d62ef756e53b7e3c
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-07-22 22:47:49 +0600

    Join User table name,Job table Job_title with JobHistory model and add condition to filter search resualt of jobHistory

--- app/Models/JobHistory.php
+++ app/Models/JobHistory.php
@@ -13,10 +13,31 @@ class JobHistory extends Model
     protected $table = 'job_history'; //db table name
 
     static public function getRecord($request) {
-        $return = self::select('job_history.*')->orderBy('id', 'DESC')->paginate(20);
-        return $return;
+    
+        $return = self::select('job_history.*', 'users.name', 'jobs.job_title')
+            ->join('users', 'users.id', '=', 'job_history.employee_id')
+            ->join('jobs', 'jobs.id', '=', 'job_history.job_id')
+            ->orderBy('job_history.id', 'DESC');
 
-        
+        if(!empty(Request::get('id'))) {
+            $return = $return->where('job_history.id', '=', Request::get('id'));
+        }
+        if(!empty(Request::get('name'))) {
+            $return = $return->where('users.name', 'like', '%'.Request::get('name').'%');
+        }
+        if(!empty(Request::get('start_date'))) {
+            $return = $return->where('job_history.start_date', '=', Request::get('start_date'));
+        }
+        if(!empty(Request::get('end_date'))) {
+            $return = $return->where('job_history.end_date', '=', Request::get('end_date'));
+        }
+        if(!empty(Request::get('job_title'))) {
+            $return = $return->where('jobs.job_title', '=', Request::get('job_title'));
+        }
+            
+        $return = $return->paginate(20);
+
+        return $return;
 
     }
  
