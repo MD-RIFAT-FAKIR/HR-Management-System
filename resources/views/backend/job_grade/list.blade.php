58416a92ffbda5d011736c51c20d1a65d820d890
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-07-23 23:33:22 +0600

    Add custom serarch in 'Job grade list' page for filtering job grade list

--- resources/views/backend/job_grade/list.blade.php
+++ resources/views/backend/job_grade/list.blade.php
@@ -9,10 +9,10 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Job History</h1>
+            <h1>Job Grade</h1>
           </div><!-- /.col -->
           <div class="col-sm-6" style="text-align: right;">
-            <a href="{{ url('admin/job_history/add') }}" class="btn btn-primary">Add Job History</a>
+            <a href="{{ url('admin/job_grades/add') }}" class="btn btn-primary">Add Job Grade</a>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
@@ -24,12 +24,50 @@
         <div class="row mt-3">
           <section class="col-md-12">
             <div class="card">
-              
+              <div class="card-header">
+                <h3 class="card-title">Search Job Grade</h3>
+              </div>
+              <form method="get" action="">
+                <div class="card-body">
+                  <div class="row">
+
+                    <div class="form-gorup col-md-3">
+                      <label>ID</label>
+                      <input type="text" name="id" value="{{ Request()->id }}" class="form-control" placeholder="Id">
+                    </div>
+                    <div class="form-gorup col-md-3">
+                      <label>Grade Level</label>
+                      <input type="text" name="grade_level" value="{{ Request()->grade_level }}" class="form-control" placeholder="Grade Level">
+                    </div>
+                    <div class="form-gorup col-md-3">
+                      <label>Lowest Sal</label>
+                      <input type="number" name="lowest_sal" value="{{ Request()->lowest_sal }}" class="form-control" placeholder="Lowest Salary">
+                    </div>
+                    <div class="form-gorup col-md-3">
+                      <label>Highest Sal</label>
+                      <input type="number" name="highest_sal" value="{{ Request()->highest_sal }}" class="form-control" placeholder="Highest Salary">
+                    </div>
+                    <div class="form-gorup col-md-3">
+                      <label>Created Date</label>
+                      <input type="date" name="created_at" value="{{ Request()->created_at }}" class="form-control">
+                    </div>
+                    <div class="form-gorup col-md-3">
+                      <label>Updated Date</label>
+                      <input type="date" name="updated_at" value="{{ Request()->updated_at }}" class="form-control">
+                    </div>
+                    <div class="form-gorup col-md-3">
+                      <button type="submit" class="btn btn-primary" style="margin-top: 30px;">Search</button>
+                      <a href="{{ url('admin/job_grades') }}" class="btn btn-success" style="margin-top: 30px;">Reset</a>
+                    </div>
+
+                  </div>
+                </div>
+              </form>
             </div>
             @include('message')
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Job History</h3>
+                <h3 class="card-title">Job Grade</h3>
               </div>
               <div class="card-body p-0">
                 <div class="card-body p-0">
