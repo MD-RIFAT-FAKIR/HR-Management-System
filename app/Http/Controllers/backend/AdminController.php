d10e541fdbe24be60332149612cb58812834bc82
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-08-02 21:58:54 +0600

    Display dynamic card on admin dashboard

--- app/Http/Controllers/backend/AdminController.php
+++ app/Http/Controllers/backend/AdminController.php
@@ -5,11 +5,33 @@ namespace App\Http\Controllers\backend;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Job;
+use App\Models\JobHistory;
+use App\Models\Region;
+use App\Models\Country;
+use App\Models\Location;
+use App\Models\Department;
+use App\Models\Manager;
+use App\Models\PayRoll;
+use App\Models\Position;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view('backend.dashboard.list');
+        $data['employeeCount'] = User::count();
+        $data['hrCount'] = User::where('is_role', '=', 1)->count();
+        $data['empCount'] = User::where('is_role', '=', 0)->count();
+        $data['jobCount'] = Job::count();
+        $data['jobHistoryCount'] = JobHistory::count();
+        $data['regionCount'] = Region::count();
+        $data['countryCount'] = Country::count();
+        $data['locationCount'] = Location::count();
+        $data['departmentCount'] = Department::count();
+        $data['managerCount'] = Manager::count();
+        $data['payRollCount'] = PayRoll::count();
+        $data['positionCount'] = Position::count();
+
+        return view('backend.dashboard.list', $data);
     }
 }
--- resources/views/backend/dashboard/list.blade.php
+++ resources/views/backend/dashboard/list.blade.php
@@ -31,14 +31,14 @@
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
+                <h3>{{ !empty($employeeCount) ? $employeeCount : '0' }}</h3>
 
-                <p>New Orders</p>
+                <p>Employees</p>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ url('admin/employees') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -46,14 +46,14 @@
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                <h3>{{!empty( $hrCount) ?  $hrCount : '0' }}</h3>
 
-                <p>Bounce Rate</p>
+                <p>Total HR</p>
               </div>
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ url('admin/employees') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -61,14 +61,14 @@
             <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-                <h3>44</h3>
+                <h3>{{ !empty($empCount) ? $empCount : '0' }}</h3>
 
-                <p>User Registrations</p>
+                <p>Total Employees</p>
               </div>
               <div class="icon">
                 <i class="ion ion-person-add"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ url('admin/employees') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -76,17 +76,130 @@
             <!-- small box -->
             <div class="small-box bg-danger">
               <div class="inner">
-                <h3>65</h3>
+                <h3>{{ !empty($jobCount) ?  $jobCount : '0'}}</h3>
 
-                <p>Unique Visitors</p>
+                <p>Jobs</p>
               </div>
               <div class="icon">
                 <i class="ion ion-pie-graph"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ url('admin/jobs') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>{{ !empty($jobHistoryCount) ? $jobHistoryCount : '0' }}</h3>
+
+                <p>Job History</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="{{ url('admin/job_history') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>{{!empty( $regionCount) ?  $regionCount : '0' }}</h3>
+
+                <p>Total Region</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="{{ url('admin/regions ') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>{{ !empty($countryCount) ?  $countryCount : '0'}}</h3>
+
+                <p>Countries</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="{{ url('admin/countries') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>{{ !empty($locationCount) ? $locationCount : '0' }}</h3>
+
+                <p>Locations</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="{{ url('admin/locations') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>{{ !empty($departmentCount) ? $departmentCount : '0' }}</h3>
+
+                <p>Departments</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="{{ url('admin/departments') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>{{ !empty($managerCount) ? $managerCount : '0' }}</h3>
+
+                <p>Managers</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="{{ url('admin/manager') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>{{!empty( $payRollCount) ?  $payRollCount : '0' }}</h3>
+
+                <p>Pay Roll</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="{{ url('admin/payroll ') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>{{ !empty($positionCount) ? $positionCount : '0' }}</h3>
+
+                <p>Position</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="{{ url('admin/position') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
+        </div>
         </div>
         <!-- /.row -->
         <!-- Main row -->
